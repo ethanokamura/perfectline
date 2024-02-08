e62def10564289ecb13a7d4d16d897578d285d0c
Ethan Okamura <ethanokamura@Ethans-Macbook-Pro.local>
2024-02-07 17:09:54 -0800

    added build directory

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,8 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
-  // site: 'https://codesantacruz.com',
-  // base: '/', // ethanokamura/
+  site: 'https://codesantacruz.com',
+  base: '/', // ethanokamura/
   output: 'static',
   integrations: [tailwind()]
 });
