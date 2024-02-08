25c9261b929ed9aaf14cb8a03a761fcf16d4de4e
Ethan Okamura <ethanokamura@Ethans-Macbook-Pro.local>
2024-02-07 17:33:39 -0800

    tailwind JIT

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 const plugin = require('tailwindcss/plugin');
 export default {
+  mode: 'jit',
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
