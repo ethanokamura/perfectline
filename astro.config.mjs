2d5fa232078b324ea0d2398dd49e2867775c81be
Ethan Okamura <ethanokamura@Ethans-Macbook-Pro.local>
2024-02-07 17:14:11 -0800

    preparing for build

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,8 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://codesantacruz.com',
-  base: '/', // ethanokamura/
-  output: 'static',
+  // site: 'https://paperwrld.github.io',
+  // base: '/codesantacruz_v2',
+  // output: 'static',
   integrations: [tailwind()]
 });
