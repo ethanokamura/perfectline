e2f832a9375804e4604f7d349175a5706f7cd997
Ethan <ethanokamura3@gmail.com>
2024-08-02 23:12:02 -0700

    minor theme fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,27 @@ import mdx from "@astrojs/mdx";
 
 import icon from "astro-icon";
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://perfectline.io',
-  // site: 'https://paperwrld.github.io',
-  // base: '/codesantacruz_v2',
-  // output: 'static',
-  integrations: [tailwind(), mdx(), icon()]
+  integrations: [tailwind(), mdx(), icon()],
+  markdown: {
+    shikiConfig: {
+      // Choose from Shiki's built-in themes (or add your own)
+      // https://shiki.style/themes
+      theme: 'catppuccin-frappe',
+      // Disable the default colors
+      // https://shiki.style/guide/dual-themes#without-default-color
+      // (Added in v4.12.0)
+      defaultColor: false,
+      // Add custom languages
+      // Note: Shiki has countless langs built-in, including .astro!
+      // https://shiki.style/languages
+      langs: [],
+      // Enable word wrap to prevent horizontal scrolling
+      wrap: true,
+      // Add custom transformers: https://shiki.style/guide/transformers
+      // Find common transformers: https://shiki.style/packages/transformers
+      transformers: [],
+    },
+  },
 });
