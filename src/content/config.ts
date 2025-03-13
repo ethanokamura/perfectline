3f2768b0a394caad0a5e33ab663d5e16c637b524
Ethan <ethanokamura3@gmail.com>
2025-03-13 16:25:52 -0700

    updated config for new courses

--- src/content/config.ts
+++ src/content/config.ts
@@ -45,8 +45,8 @@ const infoCollection = defineCollection({
 // 3. Export a single `collections` object to register your collection(s)
 //    This key should match your collection directory name in "src/content"
 export const collections = {
-  'bst': pageCollection,
   'trees': pageCollection,
+  'graphs': pageCollection,
   'courses': courseCollection,
   'cpp-101': pageCollection,
   'linked-list': pageCollection,
